e40aeb71fd24a497d375012c3816f5ea2d86268a
Arim <arim.isakovic2005@gmail.com>
2022-08-27 13:45:19 +0200

    Set up React and Tailwind

---
+++ src/Website.js
@@ -0,0 +1,9 @@
+import Header from "./components/Header";
+
+function Website() {
+  return (
+    <Header />
+  );
+}
+
+export default Website;
---
+++ src/components/Header.js
@@ -0,0 +1,3 @@
+export default function Header() {
+    return <h1 className="text-center text-9xl text-white">Isakovic Arim</h1>;
+}
